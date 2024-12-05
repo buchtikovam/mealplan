d587fe2980c0aeb509107203f9ccbcb135958dfd
monicka <buchtikovam.dev@gmail.com>
2024-12-06 00:40:17 +0100

    page startup

---
+++ src/lib/components/DayButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+
+
+
+</script>
+
+<button class="size-12 min-w-12 bg-orange-100 rounded-lg text-lg text-orange-400 font-bold">
+    Po
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import DayButton from "$lib/components/DayButton.svelte";
+</script>
+
+<div class="h-dvh w-dvh bg-violet-50 py-8 md:py-12">
+    <div class="w-[90%] max-w-[1000px] mx-auto h-full">
+        <div class="flex flex-col text-violet-950">
+            <h1 class="text-2xl md:text-3xl font-bold mb-4">Vítej v BuchtišnPro, človíčku!</h1>
+            <p>Asi by tě zajímalo, co budeme vařit?</p>
+        </div>
+
+        <div class="mt-4">
+            <p class="font-bold text-xl text-violet-950">Jídelníček</p>
+
+            <div class="bg-white mt-2 p-4 rounded-lg flex gap-4 w-full overflow-auto">
+                <DayButton />
+                <DayButton />
+                <DayButton />
+                <DayButton />
+                <DayButton />
+                <DayButton />
+                <DayButton />
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
